c10f02a181e3b8bd1d93d033c5c6a896503a4e09
Vitalii Kalinkin <vitalii@kalinkin.email>
2026-03-04 21:41:32 -0500

    better renaming

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,53 @@
-use rand::{distributions::Alphanumeric, Rng};
+use indicatif::{ProgressBar, ProgressStyle};
+use rayon::prelude::*;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    let files = std::fs::read_dir(&args[1]).unwrap();
-    for dir_entry in files {
-        let name = dir_entry.unwrap().path().to_string_lossy().into_owned();
+    let files: Vec<_> = std::fs::read_dir(&args[1])
+        .unwrap()
+        .filter_map(|e| e.ok())
+        .filter(|e| {
+            e.path().extension().and_then(|x| x.to_str()).map_or(false, |ext| {
+                ["jpg", "mp4", "png", "webp", "jpeg", "gif"].contains(&ext)
+            })
+        })
+        .collect();
+
+    let pb = ProgressBar::new(files.len() as u64);
+    pb.set_style(
+        ProgressStyle::with_template("{bar:40} {pos}/{len} {msg}")
+            .unwrap()
+            .progress_chars("=> "),
+    );
+
+    files.par_iter().for_each(|dir_entry| {
+        let name = dir_entry.path().to_string_lossy().into_owned();
         let file = std::path::Path::new(&name);
-        if file.extension().is_none() {
-            continue;
-        }
         let ext = file.extension().unwrap().to_str().unwrap();
-        if ["jpg", "mp4", "png", "webp", "jpeg", "gif"].contains(&ext) {
-            let new_name: String = rand::thread_rng()
-                .sample_iter(&Alphanumeric)
-                .take(20)
-                .map(char::from)
-                .collect();
-            let new_full_name = file.with_file_name(format!("{}.{}", new_name, ext));
-            let _ = std::fs::rename(&name, &new_full_name);
-            println!("{} -> {}", name, new_full_name.display());
+        let stem = file.file_stem().and_then(|s| s.to_str()).unwrap_or("");
+        if stem.len() == 32 && stem.chars().all(|c| c.is_ascii_hexdigit()) {
+            pb.set_message(format!("skip: {}.{}", stem, ext));
+            pb.inc(1);
+            return;
         }
-    }
+        let content = std::fs::read(&name).unwrap();
+        let hash = format!("{:x}", md5::compute(&content));
+        let new_full_name = file.with_file_name(format!("{}.{}", hash, ext));
+        let new_file_name = format!("{}.{}", hash, ext);
+        if file == new_full_name {
+            pb.set_message(format!("skip: {}", new_file_name));
+        } else {
+            match std::fs::rename(&name, &new_full_name) {
+                Ok(_) => pb.set_message(format!("rename: {}", new_file_name)),
+                Err(e) if e.kind() == std::io::ErrorKind::AlreadyExists => {
+                    std::fs::remove_file(&name).unwrap();
+                    pb.set_message(format!("duplicate: {}", file.file_name().unwrap().to_string_lossy()));
+                }
+                Err(e) => panic!("{}", e),
+            }
+        }
+        pb.inc(1);
+    });
+
+    pb.finish_with_message("done");
 }
