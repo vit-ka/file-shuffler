9e27b560b1c8b6af998f14c168a972a0e1c74ba4
Vitalii Kalinkin <vitalii@kalinkin.email>
2026-03-09 17:14:31 -0400

    show more stuff

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use indicatif::{ProgressBar, ProgressStyle};
+use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
 use rayon::prelude::*;
 
 fn main() {
@@ -13,7 +13,8 @@ fn main() {
         })
         .collect();
 
-    let pb = ProgressBar::new(files.len() as u64);
+    let mp = MultiProgress::new();
+    let pb = mp.add(ProgressBar::new(files.len() as u64));
     pb.set_style(
         ProgressStyle::with_template("{bar:40} {pos}/{len} {msg}")
             .unwrap()
@@ -25,27 +26,43 @@ fn main() {
         let file = std::path::Path::new(&name);
         let ext = file.extension().unwrap().to_str().unwrap();
         let stem = file.file_stem().and_then(|s| s.to_str()).unwrap_or("");
+
+        let spinner = mp.add(ProgressBar::new_spinner());
+        spinner.set_style(ProgressStyle::with_template("  {spinner} {msg}").unwrap());
+        spinner.set_message(format!("checking name: {}.{}", stem, ext));
+
         if stem.len() == 32 && stem.chars().all(|c| c.is_ascii_hexdigit()) {
-            pb.set_message(format!("skip: {}.{}", stem, ext));
+            spinner.set_message(format!("skip: {}.{}", stem, ext));
+            mp.remove(&spinner);
             pb.inc(1);
             return;
         }
+
+        spinner.set_message(format!("reading: {}.{}", stem, ext));
         let content = std::fs::read(&name).unwrap();
+
+        spinner.set_message(format!("hashing: {}.{}", stem, ext));
         let hash = format!("{:x}", md5::compute(&content));
         let new_full_name = file.with_file_name(format!("{}.{}", hash, ext));
         let new_file_name = format!("{}.{}", hash, ext);
+
         if file == new_full_name {
-            pb.set_message(format!("skip: {}", new_file_name));
+            spinner.set_message(format!("skip (already hashed): {}", new_file_name));
         } else {
             match std::fs::rename(&name, &new_full_name) {
-                Ok(_) => pb.set_message(format!("rename: {}", new_file_name)),
+                Ok(_) => spinner.set_message(format!("renaming: {}.{} -> {}", stem, ext, new_file_name)),
                 Err(e) if e.kind() == std::io::ErrorKind::AlreadyExists => {
                     std::fs::remove_file(&name).unwrap();
-                    pb.set_message(format!("duplicate: {}", file.file_name().unwrap().to_string_lossy()));
+                    spinner.set_message(format!(
+                        "duplicate, deleting: {}",
+                        file.file_name().unwrap().to_string_lossy()
+                    ));
                 }
                 Err(e) => panic!("{}", e),
             }
         }
+
+        mp.remove(&spinner);
         pb.inc(1);
     });
 
